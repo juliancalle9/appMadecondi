c1300d377954467915e5b12e6ced782cfe19f18a
Julian Calle Velasquez <juliancalle9@gmail.com>
2021-03-05 12:57:48 -0500

    menu con icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,55 @@
-@extends('layouts.app')
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+ 
+    @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    @section('content')
+    <link href="/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <div class="container">
+        <div class="card-body">
+            <div class="grid">
+                <div>
+                    <a href="" class="linkIcono" > 
+                        Clientes
+                        <img src="{{ asset('image/clientes.png')}}" alt="icono clientes">
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Proveedores
+                        <img src="{{ asset('image/proveedores.png')}}" alt="icono proveedores">
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Productos   
+                        <img src="{{ asset('image/producto.png')}}" alt="icono proveedores">
+                    </a>
+                </div> 
+                <div>
+                    <a href="">
+                        Ventas
+                        <img src="{{ asset('image/ventas.png')}}" alt="icono ventas">
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Compras
+                        <img src="{{ asset('image/compras.png')}}" alt="icono compras">
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Informes
+                        <img src="{{ asset('image/pdf.png')}}" alt="icono informes">
+                    </a>
                 </div>
             </div>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+        
         </div>
     </div>
-</div>
-@endsection
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('menu', function () {
+    return view('menu');
+});
